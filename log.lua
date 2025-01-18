e90bf936a4a08a664e1c2066018db0093e2d8164
Jonny QUT <donnyofdooms@gmail.com>
2025-01-18 10:17:46 +1000

    Adds logging API from vanilla LUA

---
+++ log.lua
@@ -0,0 +1,25 @@
+---
+--- Created by donnyofdoom.
+--- DateTime: 17.01.25 22:24
+---
+
+---@meta
+
+---@module 'main'
+
+---Write a message to the log.
+---Log is visible when editing the LUA box and appears in the 'Errors / Log' panel.
+function log()
+
+end
+
+---Write a message to the HUD.
+---HUD messages are visible during normal play and when on the map.
+function logToHud()
+
+end
+
+---Clears your log.
+function clearLogs()
+
+end
